25e3476015f66eeca454c92d75095bc009f0f603
Leja Aprianza <72089263+lejaaprianza@users.noreply.github.com>
2023-01-09 15:06:39 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,9 +48,10 @@ streamlit.dataframe(my_data_rows)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 
 
-if not add_my_fruit:
-    streamlit.error("select fruit pleasess")
-else:
-    streamlit.write('The user entered ', add_my_fruit)
-    my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+if streamlit.button("add"):
+    if not add_my_fruit:
+        streamlit.error("select fruit pleasess")
+    else:
+        streamlit.write('The user entered ', add_my_fruit)
+        my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
